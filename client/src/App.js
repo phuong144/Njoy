85fd89b4308878f9c8f8645699e8fde69d554a93
phuong144 <patrickphuong07@gmail.com>
2021-01-22 10:38:11 -0800

    Setup React-Redux boilerplate and overall system infrastructure design

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import jwt_decode from "jwt-decode";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./redux/actions/authActions";
+import store from "./redux/store";
+/*
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import NavBar from './components/NavBar';
+import ActivityForm from './components/ActivityForm;
+import PrivateRoute from "./components/PrivateRoute";
+*/
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
+
+/*
+  React Router SetUp
+  <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={SignIn} />
+        <Route exact path="/register" component={SignUp} />
+        <Switch>
+          <PrivateRoute path="/ActivityForm" component={ActivityForm} />
+        </Switch>
+      </div>
+    </Router>
+*/
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>Njoy</p>
   );
 }
 
