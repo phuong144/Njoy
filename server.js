d5ad0c13897221ab9b3e477b976438ea39a78979
phuong144 <patrickphuong07@gmail.com>
2021-01-24 10:58:04 -0800

    Setup MongoDB Atlas + Mongoose connection to DB

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const path = require("path");
-const bodyParser = require("body-parser");
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json()); // Converts data body into JSON format (originally bodyParser)
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-app.use(bodyParser.json());
+
+// MongoDB - Mongoose Init
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
 // Middleware
+
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
