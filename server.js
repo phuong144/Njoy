de8a26014f3744fce8fcf0441b512e3b5adc3675
phuong144 <patrickphuong07@gmail.com>
2021-01-25 08:58:12 -0800

    Implemented User authentication API

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require("path");
+const passport = require("passport");
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json()); // Converts data body into JSON format (originally bodyParser)
+app.use(express.urlencoded({extended:false}))
 
 // MongoDB - Mongoose Init
 const uri = process.env.ATLAS_URI;
@@ -16,11 +18,19 @@ mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedT
 const connection = mongoose.connection;
 connection.on('error', console.error.bind(console, 'connection error:'));
 connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+  console.log("MongoDB database connection established successfully!");
 })
 
+const users = require("./routes/api/User");
 // Middleware
 
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
