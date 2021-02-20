490c1252dd6f46e77f107ec70a2cca2a9e239741
phuong144 <patrickphuong07@gmail.com>
2021-02-20 11:26:11 -0800

    Setup Schedule.js with react-redux so that data is easily parsed

--- client/src/components/dashboard/Schedule.js
+++ client/src/components/dashboard/Schedule.js
@@ -1,7 +1,54 @@
-import react from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link, withRouter } from "react-router-dom";
+import { getSchedule } from "../../redux/actions/scheduleActions";
 
-export default function Schedule(){
+export function Schedule(props){
+  // schedule will be set to a list of objects like inputList
+  const [schedule, setSchedule] = React.useState(null);
+
+  // Acts as componentDidMount, executes on component mount to get any existing schedule
+  useEffect(() => {
+    if (schedule == null) {
+      const uid = props.auth.user.id;
+      props.getSchedule(uid);
+    }
+  })
+
+  // Executes when props changes E.g. schedule is generated
+  useEffect(() => {
+    setSchedule(props.schedule.schedule);
+  }, [props])
+
   return(
-    <p>Schedule</p>
-  );
+    <div>
+      <p>Schedule Data</p>
+      <div style={{ marginTop: 20 }}>{JSON.stringify(schedule)}</div>
+    </div>
+  )
 }
+
+// getSchedule is an action needed to retrieve any existing schedule
+// auth is an object in the redux store that stores user authentication info
+// schedule is an object in the redux store that stores the schedule data
+// errors is an object in the redux store that stores errors
+Schedule.propTypes = {
+  getSchedule: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+  schedule: PropTypes.object.isRequired,
+};
+
+// Maps redux store state to props
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors,
+  schedule: state.schedule,
+});
+
+// Connects required actions and props to this component
+export default connect(
+  mapStateToProps,
+  { getSchedule }
+)(withRouter(Schedule));
