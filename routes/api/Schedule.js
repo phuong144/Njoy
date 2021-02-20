cdd179047090290732dbf149daaa9c4822656ccf
phuong144 <patrickphuong07@gmail.com>
2021-02-20 11:33:01 -0800

    Defined function in schedule api for calculating a schedule

--- routes/api/Schedule.js
+++ routes/api/Schedule.js
@@ -6,6 +6,13 @@ require('dotenv').config();
 // const User = require("../../models/User");
 const Schedule = require("../../models/Activity");
 
+/**
+ * 
+ * @param {*} schedule list of objects [{activity:'', duration:''}]
+ */
+function calculateSchedule(schedule) {
+  
+}
 
 router.post("/generate", (req, res) => {
   // req.body.id = users id
@@ -16,6 +23,8 @@ router.post("/generate", (req, res) => {
     if (schedule) {
       // if schedule found, add onto existing activites and schedule
       schedule.activities = activities;
+
+      // set schedule.schedule to calculated available schedule
       schedule.schedule = activities;
       schedule
         .save()
